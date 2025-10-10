2d5b43fd147cc7faabd55dafe8f46d67a1b8af2c
zihao.chen <zihao.chen@xilancloud.com>
2025-10-10 11:17:40 +0800

    feat:category section update

--- src/views/WallpaperCategory.vue
+++ src/views/WallpaperCategory.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { Image } from 'primevue';
+import { Galleria } from 'primevue';
 import { computed, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import { getServerSource } from '../utils';
 import { useMyFetch } from '../utils/request';
 
 const route = useRoute()
-const category = computed(()=>route.params.category) 
+const category = computed(() => route.params.category)
 // 监听 route.params.category 变化
 const url = computed(() => `/api/category/${category.value}`)
-const { isFetching, error, data ,execute} = useMyFetch(url).json()
+const { isFetching, error, data, execute } = useMyFetch(url).json()
 // 监听 category 变化
 watch(
   () => route.params.category,
@@ -17,19 +17,77 @@ watch(
     execute()
   }
 )
+
+import { ref } from "vue";
+
+
+
+const images = ref([{
+    itemImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria1.jpg',
+    thumbnailImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria1s.jpg',
+    alt: 'Description for Image 1',
+    title: 'Title 1'
+},{
+    itemImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria1.jpg',
+    thumbnailImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria1s.jpg',
+    alt: 'Description for Image 1',
+    title: 'Title 1'
+},{
+    itemImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria1.jpg',
+    thumbnailImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria1s.jpg',
+    alt: 'Description for Image 1',
+    title: 'Title 1'
+},{
+    itemImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria1.jpg',
+    thumbnailImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria1s.jpg',
+    alt: 'Description for Image 1',
+    title: 'Title 1'
+},]);
+const activeIndex = ref(0);
+const responsiveOptions = ref([
+    {
+        breakpoint: '1024px',
+        numVisible: 5
+    },
+    {
+        breakpoint: '768px',
+        numVisible: 3
+    },
+    {
+        breakpoint: '560px',
+        numVisible: 1
+    }
+]);
+const displayCustom = ref(false);
+
+const imageClick = (index:any) => {
+    activeIndex.value = index;
+    displayCustom.value = true;
+};
 </script>
 
 <template>
-    <div v-if="isFetching">loading...</div>
-    <div v-else-if="error">{{ error }}</div>
-    <div v-else>
-        <div class="grid md:grid-cols-5 mt-10">
-            <template v-for="item in data" :key="item">
-                <Image :src="getServerSource(item.source)" alt="Image" width-="100%"  preview />
-            </template>
-        </div>
-    </div>
-</template>
-<style scoped>
+  <div v-if="isFetching">loading...</div>
+  <div v-else-if="error">{{ error }}</div>
+  <div v-else>
+    <div class="card flex justify-center">
+      <Galleria v-model:activeIndex="activeIndex" v-model:visible="displayCustom" :value="data"
+        :responsiveOptions="responsiveOptions" :numVisible="7"  :circular="true"
+        :fullScreen="true" :showItemNavigators="true" :showThumbnails="false">
+        <template #item="slotProps">
+          <img :src="getServerSource(slotProps.item.source)" :alt="slotProps.item.name" style="width: 100%; display: block" />
+        </template>
+        <template #thumbnail="slotProps">
+          <img :src="getServerSource(slotProps.item.source)" :alt="slotProps.item.name" style="display: block" />
+        </template>
+      </Galleria>
 
-</style>
+      <div v-if="images" class="grid grid-cols-12 gap-4 w-full" >
+        <div v-for="(item, index) of data" :key="index" class="col-span-3">
+          <img :src="getServerSource(item.source)" :alt="item.name" class="w-full h-full bg-contain shadow-xl rounded-md"  @click="imageClick(index)" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped></style>
