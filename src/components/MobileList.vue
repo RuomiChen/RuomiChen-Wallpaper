3bd76e7de7c34b3b656b8a1ad4a5665ca29a5381
zihao.chen <zihao.chen@xilancloud.com>
2025-10-17 15:08:45 +0800

    feat

--- src/components/MobileList.vue
+++ src/components/MobileList.vue
@@ -44,7 +44,7 @@ useIntersectionObserver(
             <img :src="getServerSource(item.source)" :alt="item.title"
                 class="w-full h-full rounded-xl duration-250 shadow-[6px_6px_5px_5px_#00000080] dark:shadow-[6px_6px_5px_5px_#fff]  hover:scale-105" />
             <div @click="router.push({ name: 'WallpaperDetail', params: { id: item._id } })"
-                class="absolute bottom-20 left-1/2 -translate-x-1/2 mx-auto cursor-pointer group-hover:opacity-100 opacity-0 transition-all shadow-[0_0_10px_#fff1f1] py-3 px-8 rounded-[50px] border dark:border-white bg-[rgba(0,0,0,.6)] flex items-center gap-4">
+                class="absolute bottom-20 left-1/2 -translate-x-1/2 mx-auto cursor-pointer group-hover:opacity-100 opacity-0 transition-all  shadow-[0_0_10px_#000] dark:shadow-[0_0_10px_#fff1f1] py-3 px-8 rounded-[50px] border dark:border-white bg-[rgba(255,255,255,.8)] dark:bg-[rgba(0,0,0,.6)] flex items-center gap-4">
                 Detail
                 <i class="pi pi-arrow-down-right"></i>
             </div>
